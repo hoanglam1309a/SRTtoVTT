18f1890a9541dc7847427e01829d0fc879823584
Nguyen Hoang Lam <96652536+hoanglam1309a@users.noreply.github.com>
2022-11-29 14:21:03 +0700

    Refactor: update index.js

--- index.js
+++ index.js
@@ -9,23 +9,19 @@ app.use(cors({ origin: true }));
 
 app.get("/", async (req, res) => {
   try {
-    if (!req.query.url)
-      return res.send("SRT to VTT Proxy. Params url is required");
-
+    if (!req.query.url) return res.send("SRT to VTT Proxy. Params url is required");
     const response = await axios.get(encodeURI(req.query.url));
-
-    if (!response.headers["content-type"].startsWith("application/x-subrip") && !response.headers["content-type"].startsWith("srt"))
+    if (
+      !response.headers["content-type"].startsWith("application/x-subrip") &&
+      !response.headers["content-type"].startsWith("srt")
+    ){
       return res.status(400).send("Invalid content type");
-
+    }
     const { subtitle } = convert(response.data, ".vtt");
-
     if (!subtitle) return res.status(400).send("Cannot convert");
-
     res.setHeader("content-type", "text/vtt");
-
     res.send(subtitle);
   } catch (error) {
-    console.log(error);
     if (!res.headerSent) res.sendStatus(500);
   }
 });
