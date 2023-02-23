39f65a046d896ec9431cb00233151f1e4ed73a03
Nguyen Hoang Lam <96652536+hoanglam1309a@users.noreply.github.com>
2023-02-23 20:43:20 +0700

    Chore: remove check content type header

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ app.get("/", async (req, res) => {
   try {
     if (!req.query.url) return res.send("SRT to VTT Proxy. Params url is required");
     const response = await axios.get(encodeURI(req.query.url));
-    if (
-      !response.headers["content-type"].startsWith("application/x-subrip") &&
-      !response.headers["content-type"].startsWith("srt")
-    ){
-      return res.status(400).send("Invalid content type");
-    }
     const { subtitle } = convert(response.data, ".vtt");
     if (!subtitle) return res.status(400).send("Cannot convert");
     res.setHeader("content-type", "text/vtt");
